7ae5c0c7be513945415253b0275973d0fd54b713
Jonas Born <jonas.born@yahoo.de>
2021-12-14 14:14:03 +0100

    Added likelihoods to TNTClassifier.php

--- src/Classifier/TNTClassifier.php
+++ src/Classifier/TNTClassifier.php
@@ -21,13 +21,30 @@ class TNTClassifier
         $this->stemmer   = new NoStemmer;
     }
 
+    //From https://gist.github.com/raymondjplante/d826df05349c1d4350e0aa2d7ca01da4
+    function softmax(array $v){
+
+        //Just in case values are passed in as string, apply floatval
+        $v = array_map('exp',array_map('floatval',$v));
+        $sum = array_sum($v);
+
+        foreach($v as $index => $value) {
+            $v[$index] = $value/$sum;
+        }
+
+        return $v;
+    }
+
+    //From https://stackoverflow.com/a/54867175/9407664
     public function predict($statement)
     {
         $words = $this->tokenizer->tokenize($statement);
 
+        $best_likelihoods = [];
         $best_likelihood = -INF;
         $best_type       = '';
         foreach ($this->types as $type) {
+            $best_likelihoods[$type] = -INF;
             $likelihood = log($this->pTotal($type)); // calculate P(Type)
             $p          = 0;
             foreach ($words as $word) {
@@ -37,11 +54,15 @@ class TNTClassifier
             $likelihood += $p; // calculate P(word, Type)
             if ($likelihood > $best_likelihood) {
                 $best_likelihood = $likelihood;
+                $best_likelihoods[$type] = $likelihood;
                 $best_type       = $type;
             }
         }
+
         return [
             'likelihood' => $best_likelihood,
+            'likelihoods' => $best_likelihoods,
+            'probability' => $this->softmax($best_likelihoods),
             'label'      => $best_type
         ];
     }
